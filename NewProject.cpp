7da4795365e4ee396419982eadaf3ce1ca3c2fda
sl1kAd <80388890+sl1kAd@users.noreply.github.com>
2021-10-20 14:08:37 +0300

    Add files via upload

---
+++ NewProject.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    priority_queue <int> priority_q;  // объявляем приоритетную очередь
+
+    cout << "Введите 7 чисел: " << endl;
+
+    for (int j = 0; j < 7; j++) 
+    {
+        int a; cin >> a;
+
+        priority_q.push(a);  // добавляем элементы в очередь
+    }
+    // выводим первый
+    cout << "Первый элемент очереди: " << priority_q.top();  // элемент
+}
